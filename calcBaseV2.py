035597e0f68f14d5e320c408cf17b1d76196c420
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2025-01-15 15:35:53 +0100

    Update calcBaseV2 add Function

--- calcBaseV2.py
+++ calcBaseV2.py
@@ -36,7 +36,7 @@ tokens = [ 'NUMBER',
            'ASSIGN',
            'CALC',
            'SIMPLECALC',
-           'CONDITIONS']  + list(reserved.values())
+           'CONDITIONS','COMMA']  + list(reserved.values())
 
 t_PLUS = r'\+' 
 t_MINUS = r'-' 
@@ -46,6 +46,7 @@ t_LPAREN = r'\('
 t_RPAREN = r'\)'
 t_LBRACE = r'\{'
 t_RBRACE = r'\}'
+t_COMMA = r','
 
 t_AND = r'&'
 t_OR = r'\|'
@@ -152,6 +153,36 @@ def p_expression_number(p):
 def p_expression_name(p):
     'expression : NAME'
     p[0] = p[1]
+
+def p_statement_function(p):
+    'statement : FUNCTION NAME LPAREN params RPAREN LBRACE bloc RBRACE'
+    p[0] = ('function', p[2], p[4], ('block', p[7]))
+
+def p_statement_function_call(p):
+    'statement : NAME LPAREN args RPAREN'
+    p[0] = ('call', p[1], p[3])
+
+def p_params(p):
+    '''params : NAME COMMA params
+              | NAME
+              | empty'''
+    if len(p) == 4:
+        p[0] = [p[1]] + p[3]
+    elif len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = []
+
+def p_args(p):
+    '''args : expression COMMA args
+            | expression
+            | empty'''
+    if len(p) == 4:
+        p[0] = [p[1]] + p[3]
+    elif len(p) == 2:
+        p[0] = [p[1]]
+    else:
+        p[0] = []
 
 def p_error(p):
     print("Syntax error in input!")
